0ff7db43a5464fc91deb03465c3ae2f28a6f4107
Michael McDermott <michael@mgmcdermott.com>
2015-06-30 18:49:22 -0400

    Dockerizing

--- app/index.js
+++ app/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://readWriteUser:askQiaonan@10.91.53.62/LINCS');
+mongoose.connect('mongodb://146.203.54.165:27017/LINCS');
 var Schema = mongoose.Schema({"assay":String,"assay-info":String},{collection:"milestones"})
 var detail = mongoose.model('detail',Schema);
 
 var jsonParser = bodyParser.json({limit:'5mb'});
 var urlencodedParser = bodyParser.urlencoded({limit:'5mb',extended:false});
 
-app.use('/',express.static(__dirname + '/public'));
+app.use('/milestonesViz',express.static(__dirname + '/public'));
 
-app.get('/meta',function(req,res){
+app.get('/milestonesViz/meta',function(req,res){
 	// console.log(typeof(req.query['ids']));
 	var ids = JSON.parse(req.query['ids']);
 	var query = detail.find({'lincs_id':{$in:ids}})
